33e98f0c41eef0c7bd8de3d5185bae147a48d574
yuuuking <yuki.eda0629@gmail.com>
2019-06-27 23:53:48 +0900

    ES-Lintの警告に対応

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,10 +1,10 @@
 <template>
   <div id="app" class="row">
-    <div v-for="list in original_lists" class="col-3">
+    <div v-for="list in original_lists" v-bind:key="list.name" class="col-3">
       <h6>{{ list.name }}</h6>
       <hr />
-      
-      <div v-for="(card, index) in list.cards" class="card card-body"></div>
+
+      <div v-for="(card, index) in list.cards" v-bind:key="(card, index)" class="card card-body"></div>
     </div>
   </div>
 </template>
